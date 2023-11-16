0204affee359c24d9945d1fd7fa9591751f8b3a7
KiraUCN <116868393+KiraUCN@users.noreply.github.com>
2023-11-16 16:32:58 +0100

    + non-implemented methods to PersonDBAcces class

---
+++ WebshopRestService/WebshopData/DatabaseLayer/PersonDatabaseAccess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebshopData.ModelLayer;
+
+namespace WebshopData.DatabaseLayer
+{
+    public class PersonDatabaseAccess : IPersonAccess
+    {
+        public int CreatePerson(Person personAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeletePerson()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Person> GetPersons()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdatePerson(Person personUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
