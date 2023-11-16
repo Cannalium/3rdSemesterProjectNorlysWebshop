5b0d14704e33b90ebea8a6397fc61d1f509c8662
KiraUCN <116868393+KiraUCN@users.noreply.github.com>
2023-11-16 11:50:21 +0100

    created Person and IPerson in API model/database

---
+++ WebshopRestService/WebshopData/ModelLayer/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebshopData.ModelLayer
+{
+    public class Person
+    {
+        public Person() { }
+
+        public Person(string? firstName, string? lastName, string? email) 
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
