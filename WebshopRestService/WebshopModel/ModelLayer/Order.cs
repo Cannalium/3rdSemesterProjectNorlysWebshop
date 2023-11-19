4d3202d64e0aa93e5727e136b3bc10c8c18c8fe3
Cannalium <10218027@ucn.dk>
2023-11-19 14:15:04 +0100

    modelLayer
    Classes, attributes, properties

--- WebshopRestService/WebshopModel/ModelLayer/Order.cs
+++ WebshopRestService/WebshopModel/ModelLayer/Order.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WebshopModel.ModelLayer
-{
-    internal class Order
-    {
+namespace WebshopModel.ModelLayer {
+    public class Order {
+
+        public Order(DateTime orderDate, decimal orderPrice) {
+
+            OrderDate = orderDate;
+            OrderPrice = orderPrice;
+            
+        }
+
+        public DateTime OrderDate { get; set; }
+        public decimal OrderPrice { get; set; }
     }
 }
