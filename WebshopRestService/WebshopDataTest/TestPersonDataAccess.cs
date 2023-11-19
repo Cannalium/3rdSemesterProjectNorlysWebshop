035e4a35e7e799d7aa04fe2cf56fef29c8580e98
KiraUCN <116868393+KiraUCN@users.noreply.github.com>
2023-11-19 14:45:00 +0100

    Updated class TestPersonDataAccess

--- WebshopRestService/WebshopDataTest/TestPersonDataAccess.cs
+++ WebshopRestService/WebshopDataTest/TestPersonDataAccess.cs
@@ -36,6 +36,67 @@ namespace WebshopDataTest
             Assert.True(personsWereRead);
         }
 
+        [Fact]
+        public void TestDeletePerson()
+        {
+            //Arange
+            Person testPerson = new Person()
+            { FirstName = "Kira",
+              LastName = "Dittmer",
+              PhoneNo = "53535173",
+              Email = "kiradittmer@ucn.dk",
+              PersonType = "Admin"
+            };
+
+            int insertedId = _personAccess.CreatePerson(testPerson);
+
+            //Act
+            bool deleteResult = _personAccess.DeletePerson(insertedId);
+
+            //Assert
+            Assert.True(deleteResult);
+        }
+
+        [Fact]
+        public void TestUpdatePerson()
+        {
+            Person testPerson = new Person()
+            {
+                FirstName = "Kira",
+                LastName = "Dittmer",
+                PhoneNo = "53535173",
+                Email = "kiradittmer@ucn.dk",
+                PersonType = "Admin"
+            };
+
+            int insertedId = (_personAccess.CreatePerson(testPerson));
+
+            //Modify details
+            testPerson.PersonId = insertedId;
+            testPerson.FirstName = "Ida";
+            testPerson.PersonType = "Employee";
+
+            //Act
+            bool updateResult = _personAccess.UpdatePerson(testPerson);
+
+            //Assert
+            Assert.True(!updateResult);
+        }
+
+        [Fact]
+        public void TestGetPersonById()
+        {
+            //Arrange
+            int personIdToRetrieve = 1;
+
+            //Act
+            Person retrievedPerson = _personAccess.GetPersonById(personIdToRetrieve);   
+
+            //Assert
+            Assert.NotNull(retrievedPerson); //Assures that a person object is retrieved
+            Assert.Equal(personIdToRetrieve, retrievedPerson.PersonId); //Ensure correct person is retrieved
+        }
+
     }
 
 }
