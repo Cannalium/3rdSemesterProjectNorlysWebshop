2c9a767b38949789e005524089316eef188de0a0
KiraUCN <116868393+KiraUCN@users.noreply.github.com>
2023-11-19 16:15:57 +0100

    Delete WebshopClientWeb directory

--- WebshopClientWeb/WebshopClientWeb/WebModelLayer/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebshopClientWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
