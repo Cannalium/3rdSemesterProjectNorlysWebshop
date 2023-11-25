f54b2739a85db57c53b9c602aab821815f133537
IdaSkall <10468612@ucn.dk>
2023-11-25 16:17:21 +0100

    Properties were never assigned

--- WebshopClientDesktop/WebshopClientDesktop/ModelLayer/Product.cs
+++ WebshopClientDesktop/WebshopClientDesktop/ModelLayer/Product.cs
@@ -8,10 +8,7 @@ namespace WebshopClientDesktop.ModelLayer
 {
     public class Product
     {
-
-        public Product() { }
-
-        public Product(string? prodName, string? prodDescription, decimal prodPrice, int prodQuantity, string? prodType)
+        public Product(string prodName, string prodDescription, decimal prodPrice, int prodQuantity, string prodType)
         {
             ProdName = prodName;
             ProdDescription = prodDescription;
@@ -20,21 +17,16 @@ namespace WebshopClientDesktop.ModelLayer
             ProdType = prodType;
         }
 
-        public Product(int prodId, string? prodName, string? prodDescription, decimal prodPrice, int prodQuantity, string? prodType)
-        {
-            ProdId = prodId;
-            ProdName = prodName;
-            ProdDescription = prodDescription;
-            ProdPrice = prodPrice;
-            ProdQuantity = prodQuantity;
-            ProdType = prodType;
-        }
 
-        public int ProdId { get; set; }
-        public string? ProdName { get; private set; }
-        public string? ProdDescription { get; private set; }
+        public string ProdName { get; private set; }
+        public string ProdDescription { get; private set; }
         public decimal ProdPrice { get; private set; }
         public int ProdQuantity { get; private set; }
-        public string? ProdType { get; private set; }
+        public string ProdType { get; private set; }
+
+        public override string ToString()
+        {
+            return ProdName;
+        }
     }
 }
