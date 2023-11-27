777bec446be6e4253465f96075c26eb0e4f53498
IdaSkall <10468612@ucn.dk>
2023-11-27 09:06:53 +0100

    Retrieve product by type(Merch)

--- WebshopClientDesktop/WebshopClientDesktop/GUI/MerchUC.Designer.cs
+++ WebshopClientDesktop/WebshopClientDesktop/GUI/MerchUC.Designer.cs
@@ -29,121 +29,119 @@
         private void InitializeComponent()
         {
             merchLbl = new Label();
-            tbl_MerchProd = new TableLayoutPanel();
-            button1 = new Button();
-            button2 = new Button();
-            createProdBtn = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            button6 = new Button();
+            btnDeleteProduct = new Button();
+            btnEditMerchProducts = new Button();
+            listBoxMerchProducts = new ListBox();
+            btnGetMerchProducts = new Button();
+            lblProcessText = new Label();
+            groupListBox = new GroupBox();
+            groupBoxCreateProduct = new GroupBox();
+            btnCreateProduct = new Button();
+            groupListBox.SuspendLayout();
+            groupBoxCreateProduct.SuspendLayout();
             SuspendLayout();
             // 
             // merchLbl
             // 
             merchLbl.AutoSize = true;
             merchLbl.Font = new Font("Sans Serif Collection", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
-            merchLbl.Location = new Point(416, 36);
+            merchLbl.Location = new Point(493, 21);
             merchLbl.Name = "merchLbl";
             merchLbl.Size = new Size(170, 68);
             merchLbl.TabIndex = 0;
             merchLbl.Text = "Merch";
             // 
-            // tbl_MerchProd
+            // btnDeleteProduct
             // 
-            tbl_MerchProd.AutoScroll = true;
-            tbl_MerchProd.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
-            tbl_MerchProd.ColumnCount = 2;
-            tbl_MerchProd.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 16.2601624F));
-            tbl_MerchProd.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 83.73984F));
-            tbl_MerchProd.Location = new Point(34, 129);
-            tbl_MerchProd.Name = "tbl_MerchProd";
-            tbl_MerchProd.RowCount = 4;
-            tbl_MerchProd.RowStyles.Add(new RowStyle(SizeType.Percent, 49.62963F));
-            tbl_MerchProd.RowStyles.Add(new RowStyle(SizeType.Percent, 50.37037F));
-            tbl_MerchProd.RowStyles.Add(new RowStyle(SizeType.Absolute, 134F));
-            tbl_MerchProd.RowStyles.Add(new RowStyle(SizeType.Absolute, 113F));
-            tbl_MerchProd.Size = new Size(904, 518);
-            tbl_MerchProd.TabIndex = 2;
+            btnDeleteProduct.Location = new Point(412, 77);
+            btnDeleteProduct.Name = "btnDeleteProduct";
+            btnDeleteProduct.Size = new Size(101, 29);
+            btnDeleteProduct.TabIndex = 3;
+            btnDeleteProduct.Text = "Slet produkt";
+            btnDeleteProduct.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnEditMerchProducts
             // 
-            button1.Location = new Point(961, 127);
-            button1.Name = "button1";
-            button1.Size = new Size(132, 29);
-            button1.TabIndex = 3;
-            button1.Text = "Rediger produkt";
-            button1.UseVisualStyleBackColor = true;
+            btnEditMerchProducts.Location = new Point(412, 42);
+            btnEditMerchProducts.Name = "btnEditMerchProducts";
+            btnEditMerchProducts.Size = new Size(132, 29);
+            btnEditMerchProducts.TabIndex = 2;
+            btnEditMerchProducts.Text = "Rediger produkt";
+            btnEditMerchProducts.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // listBoxMerchProducts
             // 
-            button2.Location = new Point(961, 162);
-            button2.Name = "button2";
-            button2.Size = new Size(101, 29);
-            button2.TabIndex = 4;
-            button2.Text = "Slet produkt";
-            button2.UseVisualStyleBackColor = true;
+            listBoxMerchProducts.FormattingEnabled = true;
+            listBoxMerchProducts.ItemHeight = 20;
+            listBoxMerchProducts.Location = new Point(23, 42);
+            listBoxMerchProducts.Name = "listBoxMerchProducts";
+            listBoxMerchProducts.Size = new Size(383, 444);
+            listBoxMerchProducts.TabIndex = 4;
             // 
-            // createProdBtn
+            // btnGetMerchProducts
             // 
-            createProdBtn.Location = new Point(961, 60);
-            createProdBtn.Name = "createProdBtn";
-            createProdBtn.Size = new Size(118, 29);
-            createProdBtn.TabIndex = 5;
-            createProdBtn.Text = "Opret produkt";
-            createProdBtn.UseVisualStyleBackColor = true;
+            btnGetMerchProducts.Location = new Point(412, 457);
+            btnGetMerchProducts.Name = "btnGetMerchProducts";
+            btnGetMerchProducts.Size = new Size(114, 29);
+            btnGetMerchProducts.TabIndex = 5;
+            btnGetMerchProducts.Text = "Se produkter";
+            btnGetMerchProducts.UseVisualStyleBackColor = true;
+            btnGetMerchProducts.Click += BtnGetMerchProducts_Click;
             // 
-            // button3
+            // lblProcessText
             // 
-            button3.Location = new Point(961, 260);
-            button3.Name = "button3";
-            button3.Size = new Size(132, 29);
-            button3.TabIndex = 6;
-            button3.Text = "Rediger produkt";
-            button3.UseVisualStyleBackColor = true;
+            lblProcessText.AutoSize = true;
+            lblProcessText.Location = new Point(23, 489);
+            lblProcessText.Name = "lblProcessText";
+            lblProcessText.Size = new Size(15, 20);
+            lblProcessText.TabIndex = 6;
+            lblProcessText.Text = "..";
             // 
-            // button4
+            // groupListBox
             // 
-            button4.Location = new Point(961, 295);
-            button4.Name = "button4";
-            button4.Size = new Size(101, 29);
-            button4.TabIndex = 7;
-            button4.Text = "Slet produkt";
-            button4.UseVisualStyleBackColor = true;
+            groupListBox.Controls.Add(lblProcessText);
+            groupListBox.Controls.Add(btnGetMerchProducts);
+            groupListBox.Controls.Add(listBoxMerchProducts);
+            groupListBox.Controls.Add(btnEditMerchProducts);
+            groupListBox.Controls.Add(btnDeleteProduct);
+            groupListBox.Location = new Point(36, 107);
+            groupListBox.Name = "groupListBox";
+            groupListBox.Size = new Size(550, 514);
+            groupListBox.TabIndex = 8;
+            groupListBox.TabStop = false;
+            groupListBox.Text = "Produkter";
             // 
-            // button5
+            // groupBoxCreateProduct
             // 
-            button5.Location = new Point(961, 399);
-            button5.Name = "button5";
-            button5.Size = new Size(132, 29);
-            button5.TabIndex = 8;
-            button5.Text = "Rediger produkt";
-            button5.UseVisualStyleBackColor = true;
+            groupBoxCreateProduct.Controls.Add(btnCreateProduct);
+            groupBoxCreateProduct.Location = new Point(611, 107);
+            groupBoxCreateProduct.Name = "groupBoxCreateProduct";
+            groupBoxCreateProduct.Size = new Size(534, 514);
+            groupBoxCreateProduct.TabIndex = 9;
+            groupBoxCreateProduct.TabStop = false;
+            groupBoxCreateProduct.Text = "Opret produkt";
             // 
-            // button6
+            // btnCreateProduct
             // 
-            button6.Location = new Point(961, 434);
-            button6.Name = "button6";
-            button6.Size = new Size(101, 29);
-            button6.TabIndex = 9;
-            button6.Text = "Slet produkt";
-            button6.UseVisualStyleBackColor = true;
+            btnCreateProduct.Location = new Point(15, 457);
+            btnCreateProduct.Name = "btnCreateProduct";
+            btnCreateProduct.Size = new Size(118, 29);
+            btnCreateProduct.TabIndex = 6;
+            btnCreateProduct.Text = "Opret produkt";
+            btnCreateProduct.UseVisualStyleBackColor = true;
             // 
             // MerchUC
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            Controls.Add(button6);
-            Controls.Add(button5);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(createProdBtn);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(tbl_MerchProd);
+            Controls.Add(groupBoxCreateProduct);
+            Controls.Add(groupListBox);
             Controls.Add(merchLbl);
             Name = "MerchUC";
             Size = new Size(1178, 650);
+            groupListBox.ResumeLayout(false);
+            groupListBox.PerformLayout();
+            groupBoxCreateProduct.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -151,13 +149,13 @@
         #endregion
 
         private Label merchLbl;
-        private TableLayoutPanel tbl_MerchProd;
-        private Button button1;
-        private Button button2;
-        private Button createProdBtn;
-        private Button button3;
-        private Button button4;
-        private Button button5;
-        private Button button6;
+        private Button btnDeleteProduct;
+        private Button btnEditMerchProducts;
+        private ListBox listBoxMerchProducts;
+        private Button btnGetMerchProducts;
+        private Label lblProcessText;
+        private GroupBox groupListBox;
+        private GroupBox groupBoxCreateProduct;
+        private Button btnCreateProduct;
     }
 }
--- WebshopClientDesktop/WebshopClientDesktop/GUI/MerchUC.cs
+++ WebshopClientDesktop/WebshopClientDesktop/GUI/MerchUC.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WebshopClientDesktop.ControlLayer;
+using WebshopClientDesktop.Logging;
+using WebshopClientDesktop.ModelLayer;
 
 namespace WebshopClientDesktop.GUI
 {
     public partial class MerchUC : UserControl
     {
+        readonly ProductControl _productControl;
+        
         public MerchUC()
         {
             InitializeComponent();
+
+            _productControl = new ProductControl();
+        }
+
+        private async void BtnGetMerchProducts_Click(object sender, EventArgs e)
+        {
+            Logger.LogInfo("Button getMerch clicked");
+            string processText = "OK";
+            List<Product> fetchedProducts = await _productControl.GetAllProductsByMerchType();
+            if (fetchedProducts != null)
+            {
+                if (fetchedProducts.Count >= 1)
+                {
+                    processText = "Ok";
+                }
+                else
+                {
+                    processText = "No merch products found";
+                }
+            }
+            else
+            {
+                processText = "Failure: An error occurred";
+            }
+            lblProcessText.Text = processText;
+            listBoxMerchProducts.DataSource = fetchedProducts;
         }
     }
 }
