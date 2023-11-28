a37a260777267b8c29f09f660a3d211c294809d7
Cannalium <10218027@ucn.dk>
2023-11-28 15:53:58 +0100

    Product view

--- WebshopClientWeb/WebshopClientWeb/Model/Product.cs
+++ WebshopClientWeb/WebshopClientWeb/Model/Product.cs
@@ -2,5 +2,28 @@
 {
     public class Product
     {
+            public Product() { }
+
+            public Product(string? prodName, string? prodDescription, decimal prodPrice, int prodQuantity, string? prodType)
+            {
+                ProdName = prodName;
+                ProdDescription = prodDescription;
+                ProdPrice = prodPrice;
+                ProdQuantity = prodQuantity;
+                ProdType = prodType;
+            }
+
+            public Product(int prodId, string? prodName, string? prodDescription, decimal prodPrice, int prodQuantity, string? prodType) : this (prodName, prodDescription, prodPrice, prodQuantity, prodType)
+            {
+                ProdId = prodId;
+            }
+
+            public int ProdId { get; set; }
+            public string? ProdName { get; set; }
+            public string? ProdDescription { get; set; }
+            public decimal ProdPrice { get; set; }
+            public int ProdQuantity { get; set; }
+            public string? ProdType { get; }
+        }
     }
-}
+
