7d3c5df4cb17651413b3b25ab7f301aeb64b60b4
KiraUCN <116868393+KiraUCN@users.noreply.github.com>
2023-12-01 11:10:37 +0100

    Removed privacy

--- WebshopClientWeb/WebshopClientWeb/Controllers/HomeController.cs
+++ WebshopClientWeb/WebshopClientWeb/Controllers/HomeController.cs
@@ -20,13 +20,6 @@ namespace WebshopClientWeb.Controllers
             return View();
         }
 
-        [Authorize]
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
