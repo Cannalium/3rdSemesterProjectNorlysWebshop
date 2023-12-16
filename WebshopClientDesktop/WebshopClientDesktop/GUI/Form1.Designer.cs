874e1ee85efb058ed4a2a44c62291457862ab0b7
elynge <10011151@ucn.dk>
2023-12-16 14:20:05 +0100

    Form updated

--- WebshopClientDesktop/WebshopClientDesktop/GUI/Form1.Designer.cs
+++ WebshopClientDesktop/WebshopClientDesktop/GUI/Form1.Designer.cs
@@ -67,8 +67,9 @@
             panelHeader.Controls.Add(picBoxNorlysLogo);
             panelHeader.Dock = DockStyle.Top;
             panelHeader.Location = new Point(0, 0);
+            panelHeader.Margin = new Padding(5, 5, 5, 5);
             panelHeader.Name = "panelHeader";
-            panelHeader.Size = new Size(1240, 87);
+            panelHeader.Size = new Size(2015, 139);
             panelHeader.TabIndex = 1;
             // 
             // btnAdministration
@@ -76,9 +77,10 @@
             btnAdministration.FlatAppearance.BorderSize = 0;
             btnAdministration.FlatStyle = FlatStyle.Flat;
             btnAdministration.Font = new Font("Sans Serif Collection", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            btnAdministration.Location = new Point(143, 24);
+            btnAdministration.Location = new Point(232, 38);
+            btnAdministration.Margin = new Padding(5, 5, 5, 5);
             btnAdministration.Name = "btnAdministration";
-            btnAdministration.Size = new Size(181, 38);
+            btnAdministration.Size = new Size(395, 61);
             btnAdministration.TabIndex = 23;
             btnAdministration.Text = "Administration";
             btnAdministration.UseVisualStyleBackColor = true;
@@ -86,9 +88,10 @@
             // picBoxNorlysLogo
             // 
             picBoxNorlysLogo.Image = Properties.Resources.Norlyslogo;
-            picBoxNorlysLogo.Location = new Point(21, 24);
+            picBoxNorlysLogo.Location = new Point(34, 38);
+            picBoxNorlysLogo.Margin = new Padding(5, 5, 5, 5);
             picBoxNorlysLogo.Name = "picBoxNorlysLogo";
-            picBoxNorlysLogo.Size = new Size(125, 38);
+            picBoxNorlysLogo.Size = new Size(203, 61);
             picBoxNorlysLogo.TabIndex = 0;
             picBoxNorlysLogo.TabStop = false;
             // 
@@ -98,9 +101,10 @@
             panelMain.Controls.Add(groupBoxCreateProduct);
             panelMain.Controls.Add(groupListBox);
             panelMain.Dock = DockStyle.Fill;
-            panelMain.Location = new Point(0, 87);
+            panelMain.Location = new Point(0, 139);
+            panelMain.Margin = new Padding(5, 5, 5, 5);
             panelMain.Name = "panelMain";
-            panelMain.Size = new Size(1240, 566);
+            panelMain.Size = new Size(2015, 906);
             panelMain.TabIndex = 2;
             // 
             // groupBoxCreateProduct
@@ -121,9 +125,11 @@
             groupBoxCreateProduct.Controls.Add(lblProductName);
             groupBoxCreateProduct.Controls.Add(btnCreateProduct);
             groupBoxCreateProduct.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
-            groupBoxCreateProduct.Location = new Point(658, 27);
+            groupBoxCreateProduct.Location = new Point(1069, 43);
+            groupBoxCreateProduct.Margin = new Padding(5, 5, 5, 5);
             groupBoxCreateProduct.Name = "groupBoxCreateProduct";
-            groupBoxCreateProduct.Size = new Size(534, 514);
+            groupBoxCreateProduct.Padding = new Padding(5, 5, 5, 5);
+            groupBoxCreateProduct.Size = new Size(868, 822);
             groupBoxCreateProduct.TabIndex = 24;
             groupBoxCreateProduct.TabStop = false;
             groupBoxCreateProduct.Text = "Produktdetaljer";
@@ -131,9 +137,10 @@
             // radioBtnMerch
             // 
             radioBtnMerch.AutoSize = true;
-            radioBtnMerch.Location = new Point(106, 405);
+            radioBtnMerch.Location = new Point(172, 648);
+            radioBtnMerch.Margin = new Padding(5, 5, 5, 5);
             radioBtnMerch.Name = "radioBtnMerch";
-            radioBtnMerch.Size = new Size(79, 27);
+            radioBtnMerch.Size = new Size(127, 42);
             radioBtnMerch.TabIndex = 22;
             radioBtnMerch.TabStop = true;
             radioBtnMerch.Text = "Merch";
@@ -143,9 +150,10 @@
             // radioBtnEvent
             // 
             radioBtnEvent.AutoSize = true;
-            radioBtnEvent.Location = new Point(20, 405);
+            radioBtnEvent.Location = new Point(32, 648);
+            radioBtnEvent.Margin = new Padding(5, 5, 5, 5);
             radioBtnEvent.Name = "radioBtnEvent";
-            radioBtnEvent.Size = new Size(73, 27);
+            radioBtnEvent.Size = new Size(115, 42);
             radioBtnEvent.TabIndex = 21;
             radioBtnEvent.TabStop = true;
             radioBtnEvent.Text = "Event";
@@ -154,9 +162,10 @@
             // 
             // btnClearDetails
             // 
-            btnClearDetails.Location = new Point(441, 457);
+            btnClearDetails.Location = new Point(717, 731);
+            btnClearDetails.Margin = new Padding(5, 5, 5, 5);
             btnClearDetails.Name = "btnClearDetails";
-            btnClearDetails.Size = new Size(87, 29);
+            btnClearDetails.Size = new Size(141, 46);
             btnClearDetails.TabIndex = 20;
             btnClearDetails.Text = "Ryd";
             btnClearDetails.UseVisualStyleBackColor = true;
@@ -165,26 +174,29 @@
             // lblProcessCreate
             // 
             lblProcessCreate.AutoSize = true;
-            lblProcessCreate.Location = new Point(19, 489);
+            lblProcessCreate.Location = new Point(31, 782);
+            lblProcessCreate.Margin = new Padding(5, 0, 5, 0);
             lblProcessCreate.Name = "lblProcessCreate";
-            lblProcessCreate.Size = new Size(18, 23);
+            lblProcessCreate.Size = new Size(29, 38);
             lblProcessCreate.TabIndex = 19;
             lblProcessCreate.Text = "..";
             // 
             // lblProductType
             // 
             lblProductType.AutoSize = true;
-            lblProductType.Location = new Point(19, 372);
+            lblProductType.Location = new Point(31, 595);
+            lblProductType.Margin = new Padding(5, 0, 5, 0);
             lblProductType.Name = "lblProductType";
-            lblProductType.Size = new Size(112, 23);
+            lblProductType.Size = new Size(182, 38);
             lblProductType.TabIndex = 17;
             lblProductType.Text = "Produkt type:";
             // 
             // btnEditProduct
             // 
-            btnEditProduct.Location = new Point(162, 457);
+            btnEditProduct.Location = new Point(263, 731);
+            btnEditProduct.Margin = new Padding(5, 5, 5, 5);
             btnEditProduct.Name = "btnEditProduct";
-            btnEditProduct.Size = new Size(132, 29);
+            btnEditProduct.Size = new Size(214, 46);
             btnEditProduct.TabIndex = 2;
             btnEditProduct.Text = "Opdater";
             btnEditProduct.UseVisualStyleBackColor = true;
@@ -192,74 +204,83 @@
             // 
             // txtBoxProductQuantity
             // 
-            txtBoxProductQuantity.Location = new Point(19, 320);
+            txtBoxProductQuantity.Location = new Point(31, 512);
+            txtBoxProductQuantity.Margin = new Padding(5, 5, 5, 5);
             txtBoxProductQuantity.Name = "txtBoxProductQuantity";
-            txtBoxProductQuantity.Size = new Size(94, 30);
+            txtBoxProductQuantity.Size = new Size(150, 44);
             txtBoxProductQuantity.TabIndex = 16;
             // 
             // lblProductQuantity
             // 
             lblProductQuantity.AutoSize = true;
-            lblProductQuantity.Location = new Point(19, 297);
+            lblProductQuantity.Location = new Point(31, 475);
+            lblProductQuantity.Margin = new Padding(5, 0, 5, 0);
             lblProductQuantity.Name = "lblProductQuantity";
-            lblProductQuantity.Size = new Size(54, 23);
+            lblProductQuantity.Size = new Size(87, 38);
             lblProductQuantity.TabIndex = 15;
             lblProductQuantity.Text = "Antal:";
             // 
             // txtBoxPrice
             // 
-            txtBoxPrice.Location = new Point(19, 258);
+            txtBoxPrice.Location = new Point(31, 413);
+            txtBoxPrice.Margin = new Padding(5, 5, 5, 5);
             txtBoxPrice.Name = "txtBoxPrice";
-            txtBoxPrice.Size = new Size(94, 30);
+            txtBoxPrice.Size = new Size(150, 44);
             txtBoxPrice.TabIndex = 14;
             // 
             // lblPrice
             // 
             lblPrice.AutoSize = true;
-            lblPrice.Location = new Point(19, 235);
+            lblPrice.Location = new Point(31, 376);
+            lblPrice.Margin = new Padding(5, 0, 5, 0);
             lblPrice.Name = "lblPrice";
-            lblPrice.Size = new Size(41, 23);
+            lblPrice.Size = new Size(68, 38);
             lblPrice.TabIndex = 13;
             lblPrice.Text = "Pris:";
             // 
             // txtBoxProductDescription
             // 
-            txtBoxProductDescription.Location = new Point(19, 136);
+            txtBoxProductDescription.Location = new Point(31, 218);
+            txtBoxProductDescription.Margin = new Padding(5, 5, 5, 5);
             txtBoxProductDescription.Multiline = true;
             txtBoxProductDescription.Name = "txtBoxProductDescription";
-            txtBoxProductDescription.Size = new Size(388, 87);
+            txtBoxProductDescription.Size = new Size(628, 137);
             txtBoxProductDescription.TabIndex = 12;
             // 
             // txtBoxProductName
             // 
-            txtBoxProductName.Location = new Point(19, 77);
+            txtBoxProductName.Location = new Point(31, 123);
+            txtBoxProductName.Margin = new Padding(5, 5, 5, 5);
             txtBoxProductName.Name = "txtBoxProductName";
-            txtBoxProductName.Size = new Size(388, 30);
+            txtBoxProductName.Size = new Size(628, 44);
             txtBoxProductName.TabIndex = 11;
             // 
             // lblProductDescription
             // 
             lblProductDescription.AutoSize = true;
-            lblProductDescription.Location = new Point(19, 113);
+            lblProductDescription.Location = new Point(31, 181);
+            lblProductDescription.Margin = new Padding(5, 0, 5, 0);
             lblProductDescription.Name = "lblProductDescription";
-            lblProductDescription.Size = new Size(95, 23);
+            lblProductDescription.Size = new Size(159, 38);
             lblProductDescription.TabIndex = 10;
             lblProductDescription.Text = "Beskrivelse:";
             // 
             // lblProductName
             // 
             lblProductName.AutoSize = true;
-            lblProductName.Location = new Point(19, 51);
+            lblProductName.Location = new Point(31, 82);
+            lblProductName.Margin = new Padding(5, 0, 5, 0);
             lblProductName.Name = "lblProductName";
-            lblProductName.Size = new Size(54, 23);
+            lblProductName.Size = new Size(87, 38);
             lblProductName.TabIndex = 9;
             lblProductName.Text = "Navn:";
             // 
             // btnCreateProduct
             // 
-            btnCreateProduct.Location = new Point(19, 457);
+            btnCreateProduct.Location = new Point(31, 731);
+            btnCreateProduct.Margin = new Padding(5, 5, 5, 5);
             btnCreateProduct.Name = "btnCreateProduct";
-            btnCreateProduct.Size = new Size(137, 29);
+            btnCreateProduct.Size = new Size(223, 46);
             btnCreateProduct.TabIndex = 6;
             btnCreateProduct.Text = "Opret produkt";
             btnCreateProduct.UseVisualStyleBackColor = true;
@@ -272,9 +293,11 @@
             groupListBox.Controls.Add(listBoxProducts);
             groupListBox.Controls.Add(btnDeleteProduct);
             groupListBox.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
-            groupListBox.Location = new Point(49, 27);
+            groupListBox.Location = new Point(80, 43);
+            groupListBox.Margin = new Padding(5, 5, 5, 5);
             groupListBox.Name = "groupListBox";
-            groupListBox.Size = new Size(550, 514);
+            groupListBox.Padding = new Padding(5, 5, 5, 5);
+            groupListBox.Size = new Size(894, 822);
             groupListBox.TabIndex = 13;
             groupListBox.TabStop = false;
             groupListBox.Text = "Produktliste";
@@ -285,36 +308,40 @@
             comboBoxProducts.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBoxProducts.FormattingEnabled = true;
             comboBoxProducts.Items.AddRange(new object[] { "Alle produkter", "Events", "Merch" });
-            comboBoxProducts.Location = new Point(412, 42);
+            comboBoxProducts.Location = new Point(670, 67);
+            comboBoxProducts.Margin = new Padding(5, 5, 5, 5);
             comboBoxProducts.Name = "comboBoxProducts";
-            comboBoxProducts.Size = new Size(132, 31);
+            comboBoxProducts.Size = new Size(212, 45);
             comboBoxProducts.TabIndex = 24;
             comboBoxProducts.SelectedIndexChanged += ComboBoxProducts_SelectedIndexChanged;
             // 
             // lblProcessText
             // 
             lblProcessText.AutoSize = true;
-            lblProcessText.Location = new Point(23, 489);
+            lblProcessText.Location = new Point(37, 782);
+            lblProcessText.Margin = new Padding(5, 0, 5, 0);
             lblProcessText.Name = "lblProcessText";
-            lblProcessText.Size = new Size(18, 23);
+            lblProcessText.Size = new Size(29, 38);
             lblProcessText.TabIndex = 6;
             lblProcessText.Text = "..";
             // 
             // listBoxProducts
             // 
             listBoxProducts.FormattingEnabled = true;
-            listBoxProducts.ItemHeight = 23;
-            listBoxProducts.Location = new Point(23, 42);
+            listBoxProducts.ItemHeight = 37;
+            listBoxProducts.Location = new Point(37, 67);
+            listBoxProducts.Margin = new Padding(5, 5, 5, 5);
             listBoxProducts.Name = "listBoxProducts";
-            listBoxProducts.Size = new Size(383, 441);
+            listBoxProducts.Size = new Size(620, 670);
             listBoxProducts.TabIndex = 4;
             listBoxProducts.SelectedIndexChanged += ListBoxProducts_SelectedIndexChanged;
             // 
             // btnDeleteProduct
             // 
-            btnDeleteProduct.Location = new Point(412, 454);
+            btnDeleteProduct.Location = new Point(670, 726);
+            btnDeleteProduct.Margin = new Padding(5, 5, 5, 5);
             btnDeleteProduct.Name = "btnDeleteProduct";
-            btnDeleteProduct.Size = new Size(124, 29);
+            btnDeleteProduct.Size = new Size(202, 46);
             btnDeleteProduct.TabIndex = 3;
             btnDeleteProduct.Text = "Slet produkt";
             btnDeleteProduct.UseVisualStyleBackColor = true;
@@ -322,12 +349,13 @@
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
-            ClientSize = new Size(1240, 653);
+            ClientSize = new Size(2015, 1045);
             Controls.Add(panelMain);
             Controls.Add(panelHeader);
+            Margin = new Padding(5, 5, 5, 5);
             Name = "Form1";
             SizeGripStyle = SizeGripStyle.Show;
             StartPosition = FormStartPosition.CenterScreen;
