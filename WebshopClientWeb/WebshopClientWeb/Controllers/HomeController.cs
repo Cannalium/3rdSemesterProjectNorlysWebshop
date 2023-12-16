6253df2c74595b5dd0871342565f96a68eadeb2a
elynge <10011151@ucn.dk>
2023-12-16 18:00:59 +0100

    Errormodel removed

--- WebshopClientWeb/WebshopClientWeb/Controllers/HomeController.cs
+++ WebshopClientWeb/WebshopClientWeb/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using WebshopClientWeb.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace WebshopClientWeb.Controllers
 {
@@ -18,13 +15,6 @@ namespace WebshopClientWeb.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
