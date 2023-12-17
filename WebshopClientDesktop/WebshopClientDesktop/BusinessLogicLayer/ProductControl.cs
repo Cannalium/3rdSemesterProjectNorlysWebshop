25b9583e0646e6c84505e3519ccc55cc11ebf608
elynge <10011151@ucn.dk>
2023-12-17 08:36:21 +0100

    BusinessLogicLayer cleaned

--- WebshopClientDesktop/WebshopClientDesktop/BusinessLogicLayer/ProductControl.cs
+++ WebshopClientDesktop/WebshopClientDesktop/BusinessLogicLayer/ProductControl.cs
@@ -1,64 +1,69 @@
-﻿using WebshopClientDesktop.Logging;
-using WebshopClientDesktop.ModelLayer;
+﻿using WebshopClientDesktop.ModelLayer;
 using WebshopClientDesktop.ServiceLayer;
 
 namespace WebshopClientDesktop.BusinessLogicLayer
 {
     public class ProductControl
     {
-
         readonly IProductAccess _productAccess;
 
         public ProductControl()
         {
             _productAccess = new ProductServiceAccess();
-
         }
 
-        public async Task<List<Product>> GetAllProductsByType(string type)
+        // Creates a new product with the provided details
+        public async Task<int> CreateProduct(string prodName, string prodDescription, decimal prodPrice, int prodQuantity, string prodType)
         {
-            Logger.LogInfo($"Getting all products by type. Type: {type}");
-            return await _productAccess.GetAllProductsByType(type);
+            // Create a new Product object with the provided details
+            Product newProduct = new(prodName, prodDescription, prodPrice, prodQuantity, prodType);
 
+            // Call the data access layer to create the product
+            int insertedId = await _productAccess.CreateProduct(newProduct);
+
+            return insertedId;
         }
 
-        public async Task<List<Product>> GetAllProductsByEventType()
-        {
-            return await GetAllProductsByType("Event");
-        }
-
-        public async Task<List<Product>> GetAllProductsByMerchType()
-        {
-            return await GetAllProductsByType("Merch");
-        }
-
-        public async Task<List<Product>> GetAllProductsBySelectedType(string selectedType)
-        {
-            return await GetAllProductsByType(selectedType);
-        }
-
+        // Retrieves all products
         public async Task<List<Product>> GetAllProducts()
         {
             return await _productAccess.GetAllProducts();
         }
 
-        public async Task<int> CreateProduct(string prodName, string prodDescription, decimal prodPrice, int prodQuantity, string prodType)
+        // Retrieves products of a specific type
+        public async Task<List<Product>> GetAllProductsByType(string type)
         {
-            Product newProduct = new(prodName, prodDescription, prodPrice, prodQuantity, prodType);
-            int insertedId = await _productAccess.CreateProduct(newProduct);
-            return insertedId;
+            return await _productAccess.GetAllProductsByType(type);
         }
 
-        public async Task<bool> DeleteProduct(int prodId)
+        // Retrieves all products of the "Event" type
+        public async Task<List<Product>> GetAllProductsByEventType()
         {
-            bool isDeleted = await _productAccess.DeleteProduct(prodId);
-            return isDeleted;
+            return await GetAllProductsByType("Event");
         }
 
+        // Retrieves all products of the "Merch" type
+        public async Task<List<Product>> GetAllProductsByMerchType()
+        {
+            return await GetAllProductsByType("Merch");
+        }
+
+        // Updates an existing product
         public async Task<bool> UpdateProduct(Product updatedProduct)
         {
+            // Call the data access layer to update the product
             bool isUpdated = await _productAccess.UpdateProduct(updatedProduct);
+
             return isUpdated;
+        }
+
+        // Deletes a product with the specified ID
+        public async Task<bool> DeleteProduct(int prodId)
+        {
+            // Call the data access layer to delete the product
+            bool isDeleted = await _productAccess.DeleteProduct(prodId);
+
+            return isDeleted;
         }
     }
 }
