17d6b1436642f024e730bb6146bdc5c80080d25b
elynge <10011151@ucn.dk>
2023-12-18 09:55:40 +0100

    personController updated

--- WebshopClientWeb/WebshopClientWeb/Controllers/PersonController.cs
+++ WebshopClientWeb/WebshopClientWeb/Controllers/PersonController.cs
@@ -6,34 +6,25 @@ using WebshopClientWeb.Model;
 
 namespace WebshopClientWeb.Controllers
 {
-    public class PersonController : Controller 
+    public class PersonController : Controller
     {
         private PersonDataControl _personDataControl;
 
-        public PersonController() 
+        public PersonController()
         {
             _personDataControl = new PersonDataControl();
         }
+
 
         // Displays the user profile page, retrieving the logged-in user's information from the provided email
         [Authorize]
         public async Task<IActionResult> Profile()
         {
-            // Get id logged in user
+            // Get email logged in user
             string? email = HttpContext.User.FindFirstValue(ClaimTypes.Email);
-            if (string.IsNullOrEmpty(email))
-            {
-                // If email is not available
-                return RedirectToAction("Error", "Home");
-            }
+
             // Get person through service
             Person? personFromService = await _personDataControl.GetPersonByEmail(email);
-
-            if (personFromService == null)
-            {
-                // Person not found
-                return RedirectToAction("NotFound","Home");
-            }
             return View(personFromService);
         }
     }
